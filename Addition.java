f1bf473b934b65ce590d810f766a060476811dfb
vishal9860 <vishaljare11@gmail.com>
2020-10-09 09:00:52 +0530

    3rd commit

--- Addition.java
+++ Addition.java
@@ -20,7 +20,7 @@ public class Addition {
             }
         }
 
-        System.out.println("enter the  2nd array");
+        System.out.println("enter the  2nds array");
         for(int i=0;i<rows;i++)
            {
             for(int j=0;j<cols;j++)
