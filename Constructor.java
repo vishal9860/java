ffffedda327374f0783329e05c4237fa141825ee
Vishal9860 <vishaljare11@gmail.com>
2020-11-01 08:54:47 -0800

    3rd commit

--- Constructor.java
+++ Constructor.java
@@ -3,7 +3,7 @@ public class Constructor {
     int a;
     String v;
     Constructor(){                //default constructor
-        System.out.println("this  is default  constructor");
+        System.out.println("this  is default  constructor that do not take any arguments");
     }
 
     Constructor(int x,String s){         //parameterized constructor that takes two arguments
@@ -21,7 +21,7 @@ public class Constructor {
 
      public static void main(String[] args) {
         Constructor obj1=new Constructor();
-        Constructor obj2=new Constructor(10,"vishal");
+        Constructor obj2=new Constructor(10,Goverment);
         obj2.display();
         
 
